567488973de5bb3a13a1af2449d9570c7a31a5c0
madhumithaasaravanan <49307435+madhumithaasaravanan@users.noreply.github.com>
2019-04-23 15:42:49 +0530

    b47.py

---
+++ b47.py
@@ -0,0 +1,7 @@
+l=[]
+a=int(input("Enter how many number:"))
+for i in range(a):
+      num=int(input("Enter the number:))
+      l.append(num)
+print("the max num is:",max(l))
+print("the max num is:",min(l))
